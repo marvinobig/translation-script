30bc1c38c5553c481ae9820b20eca024d1431ddc
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-27 15:38:35 +0200

    create variables

--- translation-script.py
+++ translation-script.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+import sys
+import os
+import csv
+from translate import Translator
+
+
+def main():
+    # get input file
+    pathToInputFile = sys.argv[1]
+    fileExtension = os.path.splitext(pathToInputFile)[1]
+    ankiFile = "./anki.csv"
+    newWordTranslations = []
+
+
+
+
+if __name__ == "__main__":
+    main()
