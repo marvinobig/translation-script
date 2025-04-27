3cb28c14071e24e097c73164e3d3180a3a909344
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-27 16:43:21 +0200

    implement save functionality

--- translation-script.py
+++ translation-script.py
@@ -12,7 +12,9 @@ def main():
     pathToInputFile = sys.argv[1]
     fileExtension = os.path.splitext(pathToInputFile)[1]
     ankiFile = f"./anki_{uuid.uuid4()}.csv"
+    saveFile = "./save.txt"
     newWordTranslations = []
+    newWords = []
 
     # check it is a txt file
     if fileExtension != ".txt":
@@ -29,19 +31,32 @@ def main():
         # split data in file into an array
         enWords = iFile.read().split()
         cleanedEnWords = list(dict.fromkeys(enWords))
+        
+        # save new words to txt file for future comparison
+        with open(saveFile, 'a+') as sFile:
+            sFile.seek(0)
+            savedWords = sFile.read().split()
+            
+            for word in cleanedEnWords:
+                if word not in savedWords:
+                    sFile.write(f"{word}\n")
+                    newWords.append(word)
 
         # translate elements
         translator = Translator(to_lang="de")
-        
-        for word in cleanedEnWords:
-            # translate elements
-            translation = translator.translate(word)
-            newWordTranslations.append([word, translation])
-            
-        # output csv file with translations
-        with open(ankiFile, 'w', newline= '') as aFile:
-            ankiFileCsv = csv.writer(aFile)
-            ankiFileCsv.writerows(newWordTranslations)
+                
+        if newWords:    
+            for word in newWords:
+                    translation = translator.translate(word)
+                    newWordTranslations.append([word, translation])
+                
+            # output csv file with translations
+            with open(ankiFile, 'w', newline= '') as aFile:
+                ankiFileCsv = csv.writer(aFile)
+                ankiFileCsv.writerows(newWordTranslations)
+        else:
+            print("No new words to translate")
+            return -1
 
 
 if __name__ == "__main__":
