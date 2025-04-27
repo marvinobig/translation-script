655d41cf487c48f1d79115e3c9f02a8800bb2c3c
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-27 15:54:17 +0200

    translate and generate anki file

--- translation-script.py
+++ translation-script.py
@@ -24,7 +24,24 @@ def main():
         print("Input file does not exist")
         return -1
         
+    # read each line in the file
+    with open(pathToInputFile) as iFile:
+        # split data in file into an array
+        enWords = iFile.read().split()
+        cleanedEnWords = list(dict.fromkeys(enWords))
 
+        # translate elements
+        translator = Translator(to_lang="de")
+        
+        for word in cleanedEnWords:
+            # translate elements
+            translation = translator.translate(word)
+            newWordTranslations.append([word, translation])
+            
+        # output csv file with translations
+        with open(ankiFile, 'w', newline= '') as aFile:
+            ankiFileCsv = csv.writer(aFile)
+            ankiFileCsv.writerows(newWordTranslations)
 
 
 if __name__ == "__main__":
