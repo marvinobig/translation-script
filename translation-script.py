acd1cddc774ac01f28799ea49a5bb747736b1a50
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-27 18:36:34 +0200

    refactor code

--- translation-script.py
+++ translation-script.py
@@ -8,31 +8,22 @@ import string
 from translate import Translator
 
 
-def main():
-    # get input file
-    pathToInputFile = sys.argv[1]
-    language = sys.argv[2]
-    fileExtension = os.path.splitext(pathToInputFile)[1]
-    ankiFile = f"./anki_{uuid.uuid4()}.csv"
-    saveFile = "./save.txt"
-    newWordTranslations = []
-    newWords = []
-
+def performChecks(pathToInputFile, fileExtension):
     # check it is a txt file
     if fileExtension != ".txt":
-        print("Input file is not a .txt file")
-        return -1
+        raise Exception("Input file is not a .txt file")
     
     # check the file exists
     if not os.path.isfile(pathToInputFile):
-        print("Input file does not exist")
-        return -1
-        
+        raise Exception("Input file does not exist")
+
+def extractWords(pathToInputFile, saveFile):
     # read each line in the file
     with open(pathToInputFile) as iFile:
         # split data in file into an array
         text = iFile.read()
         text = text.translate(str.maketrans('', '', string.punctuation)) 
+        newWords = []
         enWords = text.split()
         cleanedEnWords = list(dict.fromkeys(enWords))
         
@@ -47,25 +38,50 @@ def main():
                 if word not in savedWords:
                     sFile.write(f"{word}\n")
                     newWords.append(word)
+            
+            return newWords
+
+def generateCsv(translator, newWords, ankiFile):
+    newWordTranslations = []
+    
+    if newWords:    
+        for word in newWords:
+                translation = translator.translate(word)
+                newWordTranslations.append([word, translation])
+            
+        # output csv file with translations
+        with open(ankiFile, 'w', newline= '') as aFile:
+            ankiFileCsv = csv.writer(aFile)
+            ankiFileCsv.writerows(newWordTranslations)
+            
+        print(f"Translated {len(newWords)} words")
+        return 0
+    else:
+        raise Exception("No new words to translate")
+
+def main():
+    try:
+        # get input file
+        pathToInputFile = sys.argv[1]
+        language = sys.argv[2]
+        fileExtension = os.path.splitext(pathToInputFile)[1]
+        ankiFile = f"./anki_{uuid.uuid4()}.csv"
+        saveFile = "./save.txt"
+
+        performChecks(pathToInputFile, fileExtension)
+            
+        newWords = extractWords(pathToInputFile, saveFile)
 
         # translate elements
         translator = Translator(to_lang=language)
-                
-        if newWords:    
-            for word in newWords:
-                    translation = translator.translate(word)
-                    newWordTranslations.append([word, translation])
-                
-            # output csv file with translations
-            with open(ankiFile, 'w', newline= '') as aFile:
-                ankiFileCsv = csv.writer(aFile)
-                ankiFileCsv.writerows(newWordTranslations)
-                
-            print(f"Translated {len(newWords)} words")
-            return 0
-        else:
-            print("No new words to translate")
-            return -1
+                    
+        generateCsv(translator, newWords, ankiFile)
+    except IndexError:
+        print("Provide all arguments e.g. <input-file> <translation-language>")
+        return -1
+    except Exception as err:
+        print(f"{err}")
+        return -1
 
 
 if __name__ == "__main__":
