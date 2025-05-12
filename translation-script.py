0cd3bdd4d2d2916174a605f0e14b9d4787886948
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-05-12 12:15:30 +0200

    correct error message

--- translation-script.py
+++ translation-script.py
@@ -88,7 +88,7 @@ def main():
                     
         generateCsv(translator, newWords, ankiFile)
     except IndexError:
-        print("Provide all arguments e.g. <input-file> <target-language-code>")
+        print("Provide all arguments e.g. <input-file> <from-language-code> <target-language-code>")
         return -1
     except Exception as err:
         print(f"{err}")
